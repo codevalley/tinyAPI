88907430533eceb832725cd806e3af85400692bb
Narayan Babu <codevalley@live.com>
2024-08-30 12:36:34 +0530

    Fix test cases around DISABLE_RATE_LIMIT env variable

--- test/controllers/concerns/rate_limitable_test.rb
+++ test/controllers/concerns/rate_limitable_test.rb
@@ -6,12 +6,19 @@ class RateLimitableTest < ActionDispatch::IntegrationTest
   setup do
     @client_token = "test_token"
     @limit = 100 # Assuming the limit for create action is 100
+    REDIS.flushdb # Clear Redis before each test
+  end
+
+  test "allows requests within rate limit" do
+    (@limit - 1).times do
+      post api_v1_payloads_path, params: { payload: { content: "Test" } }, headers: { "X-Client-Token": @client_token }
+      assert_response :success
+    end
   end
 
   test "blocks requests exceeding rate limit" do
     @limit.times do
       post api_v1_payloads_path, params: { payload: { content: "Test" } }, headers: { "X-Client-Token": @client_token }
-      assert_response :success
     end
 
     # This request should exceed the rate limit
@@ -23,7 +30,6 @@ class RateLimitableTest < ActionDispatch::IntegrationTest
   test "resets rate limit after an hour" do
     @limit.times do
       post api_v1_payloads_path, params: { payload: { content: "Test" } }, headers: { "X-Client-Token": @client_token }
-      assert_response :success
     end
 
     # This request should exceed the rate limit
@@ -32,6 +38,7 @@ class RateLimitableTest < ActionDispatch::IntegrationTest
 
     # Travel 1 hour into the future
     travel 1.hour do
+      REDIS.flushdb # Simulate Redis expiring the keys
       post api_v1_payloads_path, params: { payload: { content: "Test" } }, headers: { "X-Client-Token": @client_token }
       assert_response :success
     end
